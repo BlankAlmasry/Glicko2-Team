7867dd43e05bf935cb4f586cf5245ad98689cad9
Blank <blankuser.com@protonmail.com>
2021-08-17 17:50:00 +0200

    fixed-namespaces

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "vendor/autoload.php";
-use Blankalmasry\Glicko2\Glicko2;
-
-var_dump(Glicko2::match([new Blankalmasry\Glicko2\Rating\Rating()], [new Blankalmasry\Glicko2\Rating\Rating()], 1, 0));
-
